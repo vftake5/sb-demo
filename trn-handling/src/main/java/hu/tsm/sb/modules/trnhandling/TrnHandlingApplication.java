87a5a8654b75686b0efeeb75e836e442a220ae6f
vftake5 <ViliMisi65>
2017-11-08 15:05:48 +0100

    Initial commit

---
+++ trn-handling/src/main/java/hu/tsm/sb/modules/trnhandling/TrnHandlingApplication.java
@@ -0,0 +1,12 @@
+package hu.tsm.sb.modules.trnhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrnHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrnHandlingApplication.class, args);
+	}
+}
